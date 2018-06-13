677789218077283e89c71e9306a51a932cf3074c
mmbueno <mathmarbu@gmail.com>
2018-06-13 16:06:50 +0000

    comentario

--- src/Handler/Voluntario.hs
+++ src/Handler/Voluntario.hs
@@ -14,3 +14,8 @@ getVoluntarioR = do
     todosVoluntarios <- runDB $ selectList [] [Asc VoluntarioNome]
     sendStatusJSON ok200 (object ["resp" .= todosVoluntarios])
 
+postVoluntarioR :: Handler Value
+postVoluntarioR = do
+    voluntario <- requireJsonBody :: Handler Voluntario
+    vid <- runDB $ insert voluntario
+    sendStatusJSON created201 (object ["resp" .= fromSqlKey vid])
