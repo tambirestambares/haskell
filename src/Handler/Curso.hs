1bde29c5fabaf644224850742c6ff163bab216a6
ttavaresa <tamy_tavares12@hotmail.com>
2018-06-13 02:42:31 +0000

    postCursoR

--- src/Handler/Curso.hs
+++ src/Handler/Curso.hs
@@ -12,3 +12,9 @@ getCursoR :: Handler Value
 getCursoR = do
     todosCursos <- runDB $ selectList [] [Asc CursoNome]
     sendStatusJSON ok200 (object ["curso" .= todosCursos])
+    
+postCursoR :: Handler Value
+postCursoR = do 
+    curso <- requireJsonBody :: Handler Curso
+    cid <- runDB $ insert curso
+    sendStatusJSON created201 (object ["cursoId" .= fromSqlKey cid])
