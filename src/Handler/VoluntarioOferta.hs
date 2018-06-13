2b41da51f898ec3cb7634d2f34762fd5aa3946b6
mmbueno <mathmarbu@gmail.com>
2018-06-13 16:43:34 +0000

    getVolOfertaR

--- src/Handler/VoluntarioOferta.hs
+++ src/Handler/VoluntarioOferta.hs
@@ -26,3 +26,11 @@ getVoluntarioOfertaIdR ovid = do
     ofertav <- runDB $ get404 ovid
     sendStatusJSON ok200 (object ["resp" .= ofertav])
 
+getVolOfertaR :: VoluntarioId -> Handler Value
+getVolOfertaR vid = do
+    result <- runDB $ do
+        voluntarioOfertas <- selectList [VoluntarioOfertaVoluntarioId ==. vid] []
+        ofertas <- fmap elems . getMany $ map (voluntarioOfertaOfertaId . entityVal) voluntarioOfertas
+        hosts   <- fmap elems . getMany $ map ofertaHostId ofertas
+        return $ zip3 voluntarioOfertas ofertas hosts
+    sendStatusJSON ok200 $ object ["result" .= result]
